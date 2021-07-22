35014df958ed71b520f3b168cf3c648c797d0926
Lenivaya <xocada@gmail.com>
2021-07-22 19:53:18 +0300

    Fix

--- tests/Browser/App.test.ts
+++ tests/Browser/App.test.ts
@@ -42,7 +42,7 @@ describe('App', () => {
       nodeName,
     );
     await page.keyboard.press('Enter');
-    await page.waitForSelector('div#node', {
+    await page.waitForSelector('div.node', {
       visible: true,
     });
     // await expect(page).toClick(`li#${nodeName}`);
