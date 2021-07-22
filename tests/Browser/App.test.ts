2668abc9f5b7e1f5dba9f4084c2ea5bb3af2fc5d
Lenivaya <xocada@gmail.com>
2021-07-22 13:11:30 +0300

    Write sample browser test

---
+++ tests/Browser/App.test.ts
@@ -0,0 +1,34 @@
+import puppeteer from 'puppeteer';
+import 'expect-puppeteer';
+
+describe('App', async () => {
+  const setup = async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+
+    await page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: '',
+    });
+
+    await page.goto(
+      `file://${process.cwd()}/public/index.html`,
+    );
+
+    return {
+      page,
+      browser,
+    };
+  };
+
+  const { page, browser } = await setup();
+
+  it('Loads and renders react', async () => {
+    await expect(page).toMatch('proof of concept');
+  }, 20000);
+});
